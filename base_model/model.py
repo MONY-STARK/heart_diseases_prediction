50568b6d30ab2f2ecffd8a217d90cdb960f30857
MONY-STARK <monishprasad2606@gmail.com>
2026-01-03 14:00:04 +0530

    added version to model

--- base_model/model.py
+++ base_model/model.py
@@ -1,5 +1,5 @@
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix
 from base_model.preprocessing import preprocess_data, split_data, load_data
 from pathlib import Path
 import joblib
@@ -8,33 +8,43 @@ import joblib
 
 class LogisticRegressionModel:
 
-   def __init__(self):
-       self.model = LogisticRegression(max_iter=1000)
-       self.x_train = self.x_test = self.y_train = self.y_test = None
+    def __init__(self):
+        self.model = LogisticRegression(max_iter=1000)
+        self.x_train = self.x_test = self.y_train = self.y_test = None
+        self.version = "1.0.0"
 
 
-   def train(self, data_path):
-       data = load_data(data_path)
+    def train(self, data_path):
+        data = load_data(data_path)
     #    print("before preprocessed")
     #    data.info()
-       data = preprocess_data(data)
+        data = preprocess_data(data)
     #    print("after preprocessed")
     #    data.info()
     #    return
 
-       self.x_train, self.x_test, self.y_train, self.y_test = split_data(data)
-       self.model.fit(self.x_train, self.y_train)
+        self.x_train, self.x_test, self.y_train, self.y_test = split_data(data)
+        self.model.fit(self.x_train, self.y_train)
 
 
-   def evaluate(self):
-       y_pred = self.model.predict(self.x_test)
-       print(classification_report(self.y_test, y_pred))
+    def evaluate(self):
+        y_pred = self.model.predict(self.x_test)
+        print("------------------CLassification Report----------------------------")
+        print(classification_report(self.y_test, y_pred))
+        print("------------------Confusion Matrics--------------------------------")
+        print(confusion_matrix(self.y_test, y_pred))
 
-   def save(self, model_path="saved_models/logistic_model.pkl"):
-    path = Path(model_path)
-    path.parent.mkdir(parents=True, exist_ok=True)
 
-    joblib.dump({
-        "model": self.model
-    }, path)
+
+
+    def save(self, model_path=None):
+        if model_path is None:
+            model_path = f"saved_models/{self.version}/model.pkl"
+            
+        path = Path(model_path)
+        path.parent.mkdir(parents=True, exist_ok=True)
+
+        joblib.dump({
+            "model": self.model
+        }, path)
 
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from base_model.model import LogisticRegressionModel
+from pathlib import Path
 
 
 
 
 if __name__ == "__main__":
-    data_path = "/home/stark/Documents/Ai_project/heart_diseases_prediction/framingham.csv"
+    BASE_DIR = Path(__file__).resolve().parent
+    data_path = BASE_DIR/"data"/"framingham.csv"
+    
 
 
     model = LogisticRegressionModel()
     model.train(data_path)
     model.evaluate()
-    model.save(model_path="./saved_models/base_logistic_model.pkl")
+    model.save()
